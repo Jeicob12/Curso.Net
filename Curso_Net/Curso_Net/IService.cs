6d09d0322930828f2305f0a95a392123e607ceb9
jacobo <vocosjacoboisaac@gmail.com>
2023-10-09 14:42:51 -0300

    Agrego WCF

---
+++ Curso_Net/Curso_Net/IService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+using System.Text;
+
+namespace Curso_Net
+{
+   
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        Usuario GetUser(int userId);
+
+    }
+
+    [DataContract]
+    public class Usuario 
+    {
+        [DataMember]
+        public string Nombre { get; set;}
+        [DataMember]
+        public string Apellido { get; set;}
+    }
+
+    public class Service : IService
+    {
+        public Usuario GetUser (int userId)
+        {
+            return new Usuario()
+            {
+                Nombre = "Juan",
+                Apellido = "perez"
+            };
+        }
+    }
+}
